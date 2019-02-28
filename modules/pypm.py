c6a4cedf72721914fe3ba61c04a0d09556eb8d65
Paul Stys <paul.stys@wanadoo.fr>
2019-02-28 17:12:33 +0100

    Modif

--- modules/pypm.py
+++ modules/pypm.py
@@ -1,5 +1,10 @@
+from Bio import SeqIO
+import numpy as np
+import math
+
+
 def counting_nucleotides(dataset):
-    st = dataset.upper()
+    st = dataset.upper()  # pour être sûr que toutes les nt soient en majuscule
     return st.count('A'), st.count('C'), st.count('G'), st.count('T')
 
 
@@ -10,8 +15,8 @@ def rna(s):
     else: return s
 
 def dna_to_rna(s):
-    print("".join(dna_to_rna(s)))
-    return map(rna, s)
+    print("".join(dna_to_rna(s)))  # La méthode "".join() transforme une liste en chaine de caractères, on indique le séparateur que l'on veut entre les " "
+    return map(rna, s)  # Execute une fonction sur chaque item d'un élément itérable, ici la fonction rna sur l'item s
 
 
 
@@ -22,7 +27,8 @@ def complementaire(s):
 
 # Renvoie le complémentaire inversé de la séquence
 def complementaire_reverse(s):
-    return ''.join(reversed(list(map(complementaire, s))))
+    return ''.join(reversed(list(map(complementaire, s))))  # reverse() inverse l'ordre d'une liste
+# La méthode "".join() transforme une liste en chaine de caractères, on indique le séparateur que l'on veut entre les " "
 
 
 
@@ -36,10 +42,22 @@ def fibonnacci(n, k):
         Fn, Fn_1 = fib(Fn, Fn_1, k), Fn
     return Fn
 
+# Autre méthode
+# b = b + a
+# a = b  => ça c'est faux
+
+# # il faut faire
+# c = b + a
+# a = b
+# b = c
+# # ou / équivalent :
+# a, b = b, a + b
+
+
 
 
 # % GC d'une séquence
-def compute_GC(seq):
+def percentage_GC(seq):
     nG = seq.count('G')
     nC = seq.count('C')
     n_total = len(seq)
@@ -51,7 +69,7 @@ def compute_GC(seq):
 # Différence entre 2 séquences
 def hamming_distance(seq_1, seq_2):
     d = 0
-    for x, y in zip(seq_1, seq_2): 
+    for x, y in zip(seq_1, seq_2):  # Permet de regrouper sous la forme d'un tuple les items de listes => [a, b] et [1, 2] donne [(a, 1), (b, 2)]
         if x != y:
             d += 1
     return d
@@ -74,7 +92,7 @@ def mendelian(DD, Dr, rr):
 
 
 
-def rna_to_protein(seq_RNA):  # rajouter la fonction try en vérifiant si seq_RNA est un multiple de 3
+def rna_to_protein(seq_RNA):  # rajouter une fonction try en vérifiant si seq_RNA est un multiple de 3
     aa_prots = {"UUU": "F", "UUC": "F", "UUA": "L", "UUG": "L", "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S",
                 "UAU": "Y", "UAC": "Y", "UAA": "", "UAG": "", "UGU": "C", "UGC": "C", "UGA": "", "UGG": "W", "CUU": "L",
                 "CUC": "L", "CUA": "L", "CUG": "L", "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P", "CAU": "H",
@@ -98,7 +116,7 @@ def find_motif(seq_DNA, motif):
     stop = False
     s = ""
     while stop == False:
-        n = seq_DNA.find(motif, offset)
+        n = seq_DNA.find(motif, offset)  # str.find(motif, beg=0, end=len(string)) envoie la première position ou se trouve le motif
         if n == -1:
             stop = True
         else:
@@ -108,23 +126,60 @@ def find_motif(seq_DNA, motif):
 
 
 
+def consensus(handle):
+    sequences = []
+    for record in SeqIO.parse(handle, 'fasta'):
+         sequence = []
+         for nt in record.seq:
+              sequence.extend(nt)
+         # sequence = [nt for nt in record.seq]   ça devrait être similaire mais tenir en une ligne
+         sequences.append(sequence)
+    handle.close()
+    profile = [[0]*len(sequences)]*4
+    profile = np.zeros((4, len(sequences[0])), dtype=np.int)
+    for i,line in enumerate(sequences):
+         for j, nt in enumerate(line):
+              if nt == 'A':
+                   profile[0][j] += 1
+              elif nt == 'C':
+                   profile[1][j] += 1
+              elif nt == 'G':
+                   profile[2][j] += 1
+              elif nt == 'T':
+                   profile[3][j] += 1
+
+    consensus = ''
+    for A,C,G,T in zip(profile[0],profile[1],profile[2],profile[3]):
+         if A >= C and A >= G and A >= T:
+              consensus += 'A'
+         elif C >= A and C >= G and C >= T:
+              consensus += 'C'
+         elif G >= A and G >= C and G >= T:
+              consensus += 'G'
+         elif T >= A and T >= C and T >= G:
+              consensus += 'T'
+
+
+
 def parse_fasta(fasta):
     results = []
-    strings = fasta.strip().split('>')
+    strings = fasta.strip().split('>')  # str.strip() renvoie une copie du str dans laquelle des charactères précis ont été enlevé au début et à la fin de la string, par défaut la fonction enlève les espaces
     for s in strings:
         if len(s):
-            parts = s.split()
+            parts = s.split()  # str.split(séparateur) transforme un str en list, si le séparateur n'est pas définit, ça enlève les espaces et les retours à la ligne
             k = parts[0]
             v = ''.join(parts[1:])
-            results.append((k, v))
+            results.append((k, v))  # renvoie une liste de tuple, avec le nom = k puis la séquence = v
     return results
 
 def overlap_graph(fasta, n):
     results = []
-    dna = parse_fasta(fasta)
+    dna = parse_fasta(fasta)  # dna => liste de tuple, avec le nom = k puis la séquence = v
     for k1, v1 in dna:
+        print("k1: ", k1, " v1 : ", v1)
         for k2, v2 in dna:
-            if k1 != k2 and v1.endswith(v2[:n]):
+            print("k2 : ", k2, " v2 : ", v2)
+            if k1 != k2 and v1.endswith(v2[:n]):  # str.endswitch(suffix[, start[, end]]) retourne Vrai si la chaine se termine par le suffise spécifié, sinon revoie Faux
                 results.append((k1, k2))
     return results
 
@@ -139,3 +194,18 @@ def protein_weight(protein):
     for letter in protein:  # iterate through each letter and add weight of aa
         molecular_weight += float(aa_mass_dict[letter])
     return molecular_weight
+
+
+
+def prime_numbers(number):
+    primes = []
+    for x in range(2, number + 1):
+        primes.append(x)
+    x = 2
+    while(x <= int(math.sqrt(number))):
+        if x in primes:
+            for i in range(x*2, number+1, x):
+                    if i in primes:
+                        primes.remove(i)
+        x = x+1
+    return primes
--- pypm_test.py
+++ pypm_test.py
@@ -6,29 +6,38 @@ seq_RNA_1 = 'ACUUGCAUAUCGAUAGCUAUGACUAUCGAGCAUGCAGCGAUG'
 motif = "TAT"
 protein_1 = "TCISIAMTIEHAAM"
 
-print(pypm.counting_nucleotides(seq_DNA_1))
-print(" ".join(["%d" % x for x in pypm.counting_nucleotides(seq_DNA_1)]))
+# print(pypm.counting_nucleotides(seq_DNA_1))
+# print(" ".join(["%d" % x for x in pypm.counting_nucleotides(seq_DNA_1)]))
+#
+# # pypm.dna_to_rna(seq_DNA_1)
+#
+# # pypm.complementaire(seq_DNA_1)
+# print(pypm.complementaire_reverse(seq_DNA_1))
+#
+# print(pypm.fibonnacci(30, 2))
+#
+# print(pypm.percentage_GC(seq_DNA_1))
+#
+# print(pypm.hamming_distance(seq_DNA_1, seq_DNA_2))
+#
+# print(pypm.mendelian(float(20), float(29), float(16)))
+#
+# # pypm.rna_to_protein(pypm.dna_to_rna(seq_DNA_1))
+# print(pypm.rna_to_protein(seq_RNA_1))
+#
+# print(pypm.find_motif(seq_DNA_1, motif))
+#
+consensus, profile = pypm.consensus(open('Rosalind/00- datasets/rosalind_cons.txt', 'r'))
+print(consensus)
+print('A: ' + ' '.join(str(e) for e in profile[0]))
+print('C: ' + ' '.join(str(e) for e in profile[1]))
+print('G: ' + ' '.join(str(e) for e in profile[2]))
+print('T: ' + ' '.join(str(e) for e in profile[3]))
 
-# pypm.dna_to_rna(seq_DNA_1)
+# dataset = open('Rosalind/00- datasets/rosalind_grph.txt').read()
+# for edge in pypm.overlap_graph(dataset, 3):
+#     print(edge[0] + " " + edge[1])
 
-# pypm.complementaire(seq_DNA_1)
-print(pypm.complementaire_reverse(seq_DNA_1))
+# print(pypm.protein_weight(protein_1))
 
-print(pypm.fibonnacci(30, 2))
-
-print(pypm.compute_GC(seq_DNA_1))
-
-print(pypm.hamming_distance(seq_DNA_1, seq_DNA_2))
-
-print(pypm.mendelian(float(20), float(29), float(16)))
-
-# pypm.rna_to_protein(pypm.dna_to_rna(seq_DNA_1))
-print(pypm.rna_to_protein(seq_RNA_1))
-
-print(pypm.find_motif(seq_DNA_1, motif))
-
-dataset = open('rosalind_grph.txt').read()
-for i in pypm.overlap_graph(dataset, 3):
-    print(i[0] + " " + i[1])
-
-print(pypm.protein_weight(protein_1))
+# print(pypm.prime_numbers(100))
